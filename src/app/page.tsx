a76a1cc7ecf353df1f5a1b6cce32dce0bba1f902
Mikail Katsiev <105316523+katsiev1997@users.noreply.github.com>
2024-08-30 23:08:31 +0300

    convex db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="text-rose-500 font-bold text-2xl">
-      Hello World
-      <Button size="lg">Click me</Button>
-    </div>
-  );
+    return <AuthScreen />;
 }
